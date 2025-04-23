da91cf3e8b2d774a18945dc4f616eb9de8f57c74
trust- boaz <kwizeraboaz12@gmail.com>
2025-04-23 10:10:43 +0200

    new changes

---
+++ src/NavigationBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { Toolbar } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+
+export default function NavigationBar(){
+
+   return (
+     <Container>
+
+        <Toolbar>
+             <Typography>
+                Mammoth interactive 
+
+             </Typography>
+        </Toolbar>
+
+     </Container>
+    
+   );
+
+
+}
